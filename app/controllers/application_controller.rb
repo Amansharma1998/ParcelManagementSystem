997d91c9238b8db317aaa9eeeed610330f24311d
amansharma1998 <amans@shriffle.com>
2023-09-12 15:16:12 +0530

    control task push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include JwtToken
-  before_action :authenticate_user
-  # before_action do
-  #   ActiveStorage::Current.host = request.base_url
+  # include JwtToken
+  # before_action :authenticate_user
+  # # before_action do
+  # #   ActiveStorage::Current.host = request.base_url
+  # # end
+
+  # private
+
+  # def authenticate_user
+  #   header = request.headers['Authorization']
+  #   header = header.split(' ').last if header
+  #   begin
+  #     @decoded = jwt_decode(header)
+  #     @current_user = User.find(@decoded[:user_id])
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     render json: { errors: e.message }, status: :unauthorized
+  #   rescue JWT::DecodeError => e
+  #     render json: { errors: e.message }, status: :unauthorized
+  #   end
   # end
-
-  private
-
-  def authenticate_user
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      @decoded = jwt_decode(header)
-      @current_user = User.find(@decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { errors: e.message }, status: :unauthorized
-    end
-  end
 end
