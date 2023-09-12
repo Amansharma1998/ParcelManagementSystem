022f9a3c08a45a858ae5ae534a08c5f37e1e666d
amansharma1998 <amans@shriffle.com>
2023-09-12 16:21:42 +0530

    change

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -2,8 +2,8 @@
 
 class Parcel < ApplicationRecord
   belongs_to :train
-  belongs_to :station_from, class_name: 'Station'
-  belongs_to :station_to, class_name: 'Station'
+  belongs_to :station_from, class_name: 'Station', foreign_key: 'station_from_id'
+  belongs_to :station_to, class_name: 'Station' , foreign_key: 'station_to_id'
   has_one_attached :image
 
   validates :name, :category, :weight, :station_to, :station_from, :date, :train_id, presence: true
--- app/models/station.rb
+++ app/models/station.rb
@@ -2,6 +2,8 @@
 
 class Station < ApplicationRecord
   has_many :trains, foreign_key: 'station_from_id', class_name: 'Train'
+  has_many :trains, foreign_key: 'station_to_id', class_name: 'Train'
   has_many :parcels, foreign_key: 'station_from_id', class_name: 'Parcel'
+  has_many :parcels, foreign_key: 'station_to_id', class_name: 'Parcel'
   validates :name, presence: true
 end
